ec6a3b2d1dbba0d316c0d313d58c3383ce6c656e
mini7 <mini7@mini7s-Mac-mini.local>
2017-03-31 10:29:38 +0200

    First

---
+++ cs4_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cs4_2
+//
+//  Created by mini7 on 31/03/2017.
+//  Copyright © 2017 ssst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ cs4_2/klk.swift
@@ -0,0 +1,20 @@
+//
+//  klk.swift
+//  cs4_2
+//
+//  Created by mini7 on 31/03/2017.
+//  Copyright © 2017 ssst. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class klk:UIViewController{
+
+    @IBOutlet weak var lmc: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.lmc.text = "hello"
+    }
+}
